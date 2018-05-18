a2b6fd96e952b65ef43d0b5fe863bce5a746fb58
bbailey21us <brandonbailey21@gmail.com>
2018-05-18 08:59:47 -0400

    added age verification in javaScript

--- app.js
+++ app.js
@@ -2,7 +2,27 @@ var name ="neil";
 const state=50;
 var sum = 5 +4;
 var start= name.charAt(0);//gets the first letter of name
-
+var bill={
+    name:"bill",
+    age:21
+};
+var june={
+    name:"june",
+    age:18
+};
+var tom={
+    name:"tom",
+    age:42
+}
+var brit={
+    name:"brit",
+    age : 15
+}
+var trent={
+    name:"trent",
+    age:12
+};
+var people=[bill, june, tom, brit,trent];
 
 //checks to see if the first letter is greater than L
 if(name.charAt(0)>'l') {
@@ -29,3 +49,8 @@ checkAge("charles",21);
 checkAge("abby",27);
 checkAge("james",18);
 checkAge("john",17);
+
+for (i=0;i<people.length; i++){
+    checkAge(people[i].name,people[i].age);
+
+}
